5537a91214911d9fa373c6788f3f77fda6e5903b
Ankush Tyagi <ankush.tyagi@squareboat.com>
2025-12-29 09:49:58 +0530

    add before and after target node

---
+++ LinkedList/AddNode.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self,data):
+        self.val=data
+        self.next=None
+        
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        
+    def insert_at_end(self,data):
+        new_node=Node(data)
+        
+        if(self.tail):
+            self.tail.next=new_node
+            self.tail=new_node
+        else:
+            self.tail=new_node
+            self.head=new_node
+    
+    def insert_in_between(self,target,data):
+        new_node=Node(data)
+        current=self.head
+        while(current):
+            if(current.val==target):
+                new_node.next=current.next
+                current.next=new_node
+                return
+            else:
+                current=current.next
+                
+                
+    def print(self):
+        curr = self.head
+        while(curr):
+            print(curr.val, end="=>")         
+            curr=curr.next 
+        print("\n")           
+
+
+
+    
+ll=LinkedList()
+ll.insert_at_end(12)
+ll.insert_at_end(13)
+ll.insert_at_end(14)
+ll.insert_at_end(15)
+
+ll.print()
+
+ll.insert_in_between(12,16)
+
+ll.print()
