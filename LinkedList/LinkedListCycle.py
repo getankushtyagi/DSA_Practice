526b3475f86dac1a24660587ceb378e39708569e
Ankush Tyagi <ankush.tyagi@squareboat.com>
2025-12-29 22:31:40 +0530

    has cycle

---
+++ LinkedList/LinkedListCycle.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self,data):
+        self.value=data
+        self.next=None
+        
+
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        
+    def insert_at_beginning(self,data):
+        new_node=Node(data)
+        
+        if self.head:
+            new_node.next=self.head
+            self.head=new_node
+        else:
+            self.tail=new_node
+            self.head=new_node
+        
+    def print_list(self):
+        curr=self.head
+        while curr:
+            print(curr.value, end="=>")
+            curr=curr.next
+        print("None")
+        
+    def insert_at_end(self,data):
+        new_node=Node(data)
+        
+        if(self.tail):
+            self.tail.next=new_node
+            self.tail=new_node
+        else:
+            self.tail=new_node
+            self.head=new_node
+            
+    def hasCycle(self):
+        if(self.head is None):
+            return False
+
+        slow=fast=self.head
+
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+            if(slow==fast):
+                return True
+        return False
+    
+            
+            
+            
+ll=LinkedList()
+ll.insert_at_end(10)
+ll.insert_at_end(5)
+ll.insert_at_end(4)
+ll.insert_at_end(15)
+ll.insert_at_end(15)
+
+ll.print_list()
+
+print(ll.hasCycle())
