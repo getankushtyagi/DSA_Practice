c6c9ded8421f88c01984d3bc3ecac2e0a51150dc
Ankush Tyagi <ankush.tyagi@squareboat.com>
2026-01-11 00:01:51 +0530

    check palindrome using recursion

--- Basic/checkPalindromeString.py
+++ Basic/checkPalindromeString.py
@@ -1,8 +1,41 @@
-def palindrome(s):
-    if(s==s[::-1]):
+# without reucursion 
+# def palindrome(s):
+#     if(s==s[::-1]):
+#         return True
+#     else:
+#         return False
+
+
+    # not optimize for large string slicing fails when string is too large 
+    
+    # def palindrome( s):
+    #     # base case
+    #     if len(s) <= 1:
+    #         return True
+    
+    #     # if first and last characters don't match
+    #     if s[0] != s[-1]:
+    #         return False
+    
+    #     # recursive call
+    #     return isPalindrome(s[1:-1])
+    
+    
+def palindrome( s):
+    return check(s, 0, len(s) - 1)
+
+def check( s, left, right):
+        # base case
+    if left >= right:
         return True
-    else:
+
+        # mismatch
+    if s[left] != s[right]:
         return False
+
+    # recursive call
+    return check(s, left + 1, right - 1)
+
 
 
 print(palindrome("ankush"))
