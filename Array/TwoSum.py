21f1c09dc892cb33c14c76142cc3ae23a78d4c16
Ankush Tyagi <ankush.tyagi@squareboat.com>
2026-03-21 15:45:06 +0530

    Add two-pointer method for finding indices of two numbers that add up to target

--- Array/TwoSum.py
+++ Array/TwoSum.py
@@ -25,6 +25,26 @@ class Solution:
                     return [i,j]
                 
                 
+    # with the help of two pointers  if array is sorted
+    def twoPointer(self,nums, target):
+        i=0
+        j=len(nums)-1
+        if i==j or len(nums)==1:
+            return nums
+        
+        while i<=j:
+            if nums[i]+nums[j] == target:
+                return [i,j]
+            elif nums[i]+nums[j]<target:
+                i+=1
+            else:
+                j-=1
+        return None
+            
+                
+                
 obj=Solution()
 print(obj.twoSumOptimize([1,3,4,2,5,6,7,8],3))
 print(obj.twoSum([1,3,4,2,5,6,7,8],3))
+print(obj.twoPointer([1,2,6,7,8],8))
+print(obj.twoPointer([5,25,75],100))
